9fe2c5b969780950def3d37c6355dffaa44029c6
ReggieGrant <grantreggie09@gmail.com>
2025-10-19 16:21:51 -0700

    Final report

--- mb/posts/urls.py
+++ mb/posts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from .views import PostListView
-from .views import DetailView
+from .views import PostDetailView
+from .views import PostCreateView
+from .views import PostUpdateView
+from .views import PostDeleteView
 
 
 urlpatterns = [ 
-       path('', PostListView.as_view(), name='post_list'), 
-        path('detailed/', DetailView.as_view(), name='post_detail'),
+        path('', PostListView.as_view(), name='post_list'), 
+        path('detailed/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+        path('new/', PostCreateView.as_view(), name='post_new'),
+        path('edit/<int:pk>/', PostUpdateView.as_view(), name='post_edit'),
+        path('delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
 ]
--- mb/posts/views.py
+++ mb/posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 
 # Create your views here.
 # class based views and function based views
@@ -19,6 +21,43 @@ class PostDetailView(DetailView):
     template_name = 'posts/detail.html' # specify your template name
     model = Post # specify the model to use
     context_object_name = 'detailed' # name of the context variable in the template
+
+
+class PostCreateView(CreateView):
+    """View to create a new blog post."""
+    
+    template_name = 'posts/new.html' # specify your template name
+    model = Post # specify the model to use
+    fields = ['title', 'subtitle','body'] # fields to include in the form
+
+    def form_valid(self, form):
+        """If the form is valid, save the associated model."""
+        print(form)
+        print(User.objects.all())
+        form.instance.author = User.objects.filter(is_superuser=True).first() # set the author to the first user
+        return super().form_valid(form)
+    
+
+
+class PostUpdateView(UpdateView):
+    """View to update an existing blog post."""
+    
+    template_name = 'posts/edit.html' # specify your template name
+    model = Post # specify the model to use
+    fields = ['title', 'subtitle','body'] # fields to include in the form
+    
+
+
+class PostDeleteView(DeleteView):
+    """View to delete a blog post."""
+    
+    template_name = 'posts/delete.html' # specify your template name
+    model = Post # specify the model to use
+    success_url = reverse_lazy('post_list') # URL to redirect to after deletion
+    
+
+    
+    
     
 
    
